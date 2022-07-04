f9c624ba9381897b40f12040bdd8e1215bea3e92
mmohsin2 <62321318+mmohsin2@users.noreply.github.com>
2022-07-03 23:51:36 -0400

    Add files via upload

---
+++ tiktok-backend/data.js
@@ -0,0 +1,22 @@
+const data =[
+    {
+      url:"https://v16m-webapp.tiktokcdn-us.com/fec8b7b762720b281b37d8208488c4a1/62be8094/video/tos/useast5/tos-useast5-ve-0068c004-tx/a9e8199658ae42dc8e072f0edc812881/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2014&bt=1007&cs=0&ds=3&ft=ebtHKH-qMyq8ZcWQ7we2NHuufl7Gb&mime_type=video_mp4&qs=0&rc=NmRpOTs4Z2c0Omc0ZTU3OUBpam12aWk6Zjl1ZDMzZzczNEAyX19jNTVgX2ExY14yMjVeYSNkXzM0cjRnM2lgLS1kMS9zcw%3D%3D&l=202206302305140100020450077350020210BA11BF4",
+      channel:"flowyfairyy",
+      description:"Bloom from within",
+      song:"calm piano",
+      likes:"900",
+      messages:"456",
+      shares:"786",
+    },
+    {
+        url:"https://v16m-webapp.tiktokcdn-us.com/825fff25af1219ed6921ccffa9386553/62bf96d6/video/tos/useast5/tos-useast5-pve-0068-tx/6d86f2859efd4958be5291ec1361e7b2/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2810&bt=1405&cs=0&ds=3&ft=ebtHKH-qMyq8Z_ua7we2NWJefl7Gb&mime_type=video_mp4&qs=0&rc=ZmdkZWRkNjM1ODk6aDNoaUBpajNobTY6Zmt3ZDMzZzczNEA0LTUvNi4xXjExL18tLzEzYSNxNmpncjRvMmZgLS1kMS9zcw%3D%3D&l=20220701185224010002007637004005006003031015B2160",
+        channel:"flowyfairyy",
+        description:"Lost in deep thought",
+        song:"cry - fav sounds",
+        likes:"900",
+        messages:"456",
+        shares:"786",
+      },
+];
+
+export default data;
